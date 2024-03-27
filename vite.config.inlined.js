fde9930f9036a1ea42f8765722074109d23fc603
hafizhaah <155833744+hafizhaah@users.noreply.github.com>
2024-03-28 00:30:09 +0700

    Add files via upload

---
+++ vite.config.inlined.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from "vite-plugin-singlefile"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), viteSingleFile()],
+    build: {
+        cssCodeSplit: false,
+        assetsInlineLimit: 100000000,
+        rollupOptions: {
+            output: {
+                manualChunks: () => "everything.js",
+            },
+        },
+    },
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+    ]
+  },
+  server: {
+    open: true,
+  },
+  define: {
+    'process.env': process.env
+  }
+})
